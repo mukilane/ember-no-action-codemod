df4b9d149b957e8b542501c82ccfdb54e660023a
NRanjithkumar <46364114+NRanjithkumar@users.noreply.github.com>
2025-04-30 13:22:13 +0530

    improvement: handle component methods used as actions
    Changes added to handle the methods in the js file which used as action in hbs to actions.
    
    ---------
    
    Co-authored-by: ranjithkumar.n <ranjithkumar.n@zohocorp.com>

--- transforms/no-action-js/__testfixtures__/component.output.js
+++ transforms/no-action-js/__testfixtures__/component.output.js
@@ -1,7 +1,7 @@
 import { computed, action } from '@ember/object';
 
 export default Component.extend({
-    test() {},
+    test: action(function() {}),
     method: action(function() {}),
     anotherMethod: action(function(param) {}),
     expr: action(function() {}),
